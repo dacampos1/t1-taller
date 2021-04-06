4ca08c99990ffe34fe2e59722781b97670bce1b2
Diego Campos <dacampos1@uc.cl>
2021-04-05 20:58:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,30 +23,3 @@ function App() {
 }
 
 export default App;
-<div class="default-page">
-  <div class="header">
-    <h1 id="main-title" class="container"> Breaking Bad App</h1>
-  </div>
-  <div class="main container clearfix">
-    <div class="links">
-      <ol id="lista">
-        <h2>Better Call Saul</h2>
-      </ol>
-      <script>
-        const url = 'https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul';
-        const options = {
-          method: 'GET'
-        };
-        var lista = document.getElementById("lista");
-        fetch(url, options)
-          .then(response => response.json())
-          .then(data => data.map(
-              function append(x) { 
-                const li=document.createElement('li');
-                lista.appendChild(li);
-                li.innerHTML=li.innerHTML + x["title"] + x["season"];}));
-      </script>
-
-    </div>
-  </div>
-</div>
