382a0905a65330346b2308a5c5a98a3067396d4f
Diego Campos <dacampos1@uc.cl>
2021-04-06 15:41:06 -0400

    temporadas lista

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import './App.css';
 import apiCall from './services/api';
 import Season from './components/season';
 import React, {useState} from "react";
+import _ from 'underscore';
 
 function App() {
-  const [state, setstate] = useState([])
-  const call =apiCall("episodes?series=Better+Call+Saul");
-  call.then(x=> setstate(x))
+  const [stateSaul, setStateSaul] = useState([])
+  const callSaul =apiCall("episodes?series=Better+Call+Saul");
+  callSaul.then(x=> setStateSaul(_.groupBy(x, "season")))
+  const [stateBreaking, setStateBreaking] = useState([])
+  const callBreaking =apiCall("episodes?series=Breaking+Bad");
+  callBreaking.then(x=> setStateBreaking(_.groupBy(x, "season")))
   
+  function iterate(state){
+    if(state){
+      const keys = Object.keys(state);
+      return keys.map(x=>(<Season num={x} episodes= {state[x]}/>))
+    }
+  }
 
   return (
     <div className="App">
-      <h1>Breaking Bad</h1>
-       <Season num = "13" episodes = {state}/>
+      <h1>Breakingg Bad</h1>
+      {iterate(stateBreaking)}
       <h1>Better Call Saul</h1>
-        <Season num = "1"/>
+      {iterate(stateSaul)}
+       
     </div>
   );
 }
--- src/components/season.jsx
+++ src/components/season.jsx
@@ -1,11 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
+import _ from 'underscore';
 
 
 function Season(props) {   
+  const {num,episodes}= props;
+  const sorted_episodes =  _.sortBy(episodes,"episode_id")
   return (
     <div> 
-        <h3>Temporada {props.num}</h3>
-        {props.episodes?props.episodes.map(x=>(<h5>{x["title"]}</h5>)):<div/>}
+        <h3>Temporada {num}</h3>
+        {Array.isArray(sorted_episodes)?sorted_episodes.map(x=>(<h5 key={x["episode_id"]} >{x["episode"] + " - "+x["title"]}</h5>)):<div/>}
     </div>
   );
 }
